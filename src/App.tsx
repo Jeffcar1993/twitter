299a48dcfe1dca2b9a947d7f088e7dadbe0970b7
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-01-24 09:53:28 -0500

    ADD context para renderizar condicionalmente el componente login y ajuste de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import styles from "./App.module.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Navbar from "./Componentes/Navbar/Navbar"
 import Inicio from "./Componentes/Inicio/Inicio"
 import Login from "./Componentes/Login/Login";
+import { useAuth } from "./Componentes/Context/AuthContext";
 
 const App = () => {
+  const { isAuthenticated } = useAuth();
   return (
+
     <BrowserRouter>
 
       <div className={styles.container}>
         
-        <div className={styles.navbar}>
-          <Navbar />
-        </div>
+        { isAuthenticated && 
+          (
+            <div className={styles.navbar}>
+              <Navbar />
+            </div>
+          )
+        }
         
         <div className={styles.main}>
           <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/login" element={<Login />}/>
+            <Route path="/" element={ isAuthenticated ? <Inicio /> : <Navigate to="/login" replace />} />
+            <Route path="/login" element={ !isAuthenticated ? <Login /> : <Navigate to="/" replace />} />
           </Routes>
         </div>
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from './Componentes/Context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+
+    <AuthProvider>
+
+      <App />
+
+    </AuthProvider>
+    
   </StrictMode>,
 )
