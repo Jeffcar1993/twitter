833d5d2d44bb89665368e16f42f642cbc1686118
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-01-26 12:33:02 -0500

    UPDATE estilos para renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,48 @@
 import styles from "./App.module.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Navbar from "./Componentes/Navbar/Navbar"
-import Inicio from "./Componentes/Inicio/Inicio"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./Componentes/Navbar/Navbar";
+import Inicio from "./Componentes/Inicio/Inicio";
 import Login from "./Componentes/Login/Login";
 import { AuthProvider, useAuth } from "./Componentes/Context/AuthContext";
 import Crearcuenta from "./Componentes/Crearcuenta";
 
 const App = () => {
   const { isAuthenticated } = useAuth();
+
   return (
-
-      <AuthProvider>
-        
-          <BrowserRouter>
-
-            <div className={styles.container}>
-              
-              { isAuthenticated && 
-                (
-                  <div className={styles.navbar}>
-                    <Navbar />
-                  </div>
-                )
-              }
-              
-              <div className={styles.main}>
-                <Routes>
-                  <Route path="/" element={ isAuthenticated ? <Inicio /> : <Navigate to="/login" replace />} />
-                  <Route path="/login" element={ !isAuthenticated ? <Login /> : <Navigate to="/" replace />} />
-                  <Route path="/crearcuenta" element={ <Crearcuenta />} />
-                </Routes>
-              </div>
-
+    <AuthProvider>
+      <BrowserRouter>
+        <div
+          className={`${styles.container} ${
+            isAuthenticated ? styles.authenticated : styles.unauthenticated
+          }`}
+        >
+          {isAuthenticated && (
+            <div className={styles.navbar}>
+              <Navbar />
             </div>
+          )}
+          <div className={styles.main}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  isAuthenticated ? <Inicio /> : <Navigate to="/login" replace />
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  !isAuthenticated ? <Login /> : <Navigate to="/" replace />
+                }
+              />
+              <Route path="/crearcuenta" element={<Crearcuenta />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
-          </BrowserRouter>
-      </AuthProvider>
-
-  )
-}
-
-export default App
+export default App;
